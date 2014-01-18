e12a3b6ed024e24288a7cd5dced3a5411be4acce
Jaiglissechef-i9100 <yaggiland@hotmail.com>
2014-01-18 12:18:02 +0100

    Add combo lunch i9100

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.1 | awk {'print $1'})
 do
+    add_lunch_combo cm_i9100-userdebug
     add_lunch_combo cm_maguro-userdebug
 
 done
